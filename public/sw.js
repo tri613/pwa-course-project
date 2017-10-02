0d0c5fbb36c2ea4487df2fa6ab079993631b4551
tri613 <tri613@gmail.com>
2017-10-02 17:57:22 +0800

    add service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,67 @@
-self.addEventListener('install', function(e) {
+var CACHE_STATIC_NAME = 'static-v3';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
+self.addEventListener('install', function(event) {
     // fires on the first time install (?)
     console.info('[Serivice Worker] Installing Service Worker...');
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(function(cache) {
+                console.info('[Serivice Worker] Precaching app shell');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/images/main-image.jpg'
+                ]);
+            })
+    );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function(event) {
     // `activate` is only triggered on 'first load' or 'tab reopened' 
     // but not 'tab reload' because the page might be communicating with the old service worker
     // and might break if activate the new service worker
     console.info('[Serivice Worker] Activating Service Worker...');
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    console.log('[Serivice Worker] removing old cache');
+                    if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+                        return caches.delete(key);
+                    }
+                }));
+            })
+    );
     return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-    console.info('[Serivice Worker] fetching...');
-    e.respondWith(fetch(e.request));
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request)
+                        .then(function(res) {
+                            return Promise.all([caches.open(CACHE_DYNAMIC_NAME), res])
+                        })
+                        .then(function([cache, res]) {
+                            cache.put(event.request.url, res.clone())
+                            return res;
+                        })
+                        .catch(function(err) {
+                            // do nothing
+                        })
+                }
+            })
+    );
 });
